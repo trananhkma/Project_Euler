06647c494f8e3f1d7321bcb9cc1bf550c7bbbe55
Trần Tuấn Anh <trananhkma@gmail.com>
2015-06-01 09:10:37 +0700

    10.py

--- 10.py
+++ 10.py
@@ -1,1 +1,17 @@
-sđg
+import math
+def is_prime(x):
+    count = 1
+    while count < math.sqrt(x):
+        count += 1
+        if not x % count:
+            return False
+    return True
+
+sum = 2
+number = 3
+while number < 2000000:
+    if is_prime(number):
+        sum += number
+    number += 2
+
+print sum
