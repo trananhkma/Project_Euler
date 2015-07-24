ef699e16d8951933c94633f54f436502f8e35b66
Trần Tuấn Anh <trananhkma@gmail.com>
2015-07-24 10:39:56 +0700

    edit

--- 112.py
+++ 112.py
@@ -1,48 +1,22 @@
-def is_increasing_num(n):
-    """
-    Return True if n is increasing number
-    Return None if n is bouncy number
-    """
+def is_bouncy(n):
     s = str(n)
-    s = s[0] + s.lstrip(s[0])
-    if len(s) == 1:
-        return True
-    for i in range(len(s)):
-        if i == 1:
-            if int(s[i]) > temp:
-                increasing_num = True
-            else:
-                increasing_num = False
-        if i > 1:
-            if increasing_num == True:
-                if int(s[i]) >= temp:
-                    continue
-                else:
-                    return None
-            else:
-                if int(s[i]) <= temp:
-                    continue
-                else:
-                    return None
-        temp = int(s[i])
-    return increasing_num
-
+    inc = False
+    dec = False
+    for i in range(len(s)-1):
+        if s[i] > s[i+1]:
+            inc = True
+        if s[i] < s[i+1]:
+            dec = True
+        if inc and dec:
+            return True
+    return False
 
 if __name__ == '__main__':
-    n = 1
-    bouncys = 0
-    while n < 1000:
-        if is_increasing_num(n) is None:
-            bouncys += 1
-        n += 1
-    print bouncys
-
     count = 0
-    proportion = 50.0000000
-    # proportion = 90.000000
+    proportion = 99.000
     i = 100
     while True:
-        if is_increasing_num(i) is None:
+        if is_bouncy(i):
             count += 1
         a = float(count)*100/i
         if a == proportion:
