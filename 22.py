5775eec18ee2c01827c01ad849b15ef01f8b7ee5
Trần Tuấn Anh <trananhkma@users.noreply.github.com>
2015-09-07 00:02:15 +0700

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,13 +1,5 @@
-def p22():
-    with open('p022_names.txt') as f:
-        inter_ = f.readline().strip('"').split('","')
-        return inter_
-
-inter = p22()
-total = 0
-for i in inter:
-    total += sum((ord(j)-96) for j in i.lower()) * (sorted(inter).index(i)+1)
-
-print total
+with open('p022_names.txt') as f:
+    inter_ = f.readline().strip('"').split('","')
+print sum([sum((ord(j)-96) for j in i.lower()) * (sorted(inter).index(i)+1) for i in inter])
 
 # 871198282
