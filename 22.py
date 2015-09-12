d254d37f114cb835230438a249f2150fc50ea666
Trần Tuấn Anh <trananhkma@users.noreply.github.com>
2015-09-13 01:43:47 +0700

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,5 +1,5 @@
 with open('p022_names.txt') as f:
     _input = f.readline().strip('"').split('","')
-print sum([sum((ord(j)-96) for j in i.lower()) * (sorted(inter).index(i)+1) for i in _input])
+print sum([sum((ord(j)-96) for j in i.lower()) * (sorted(_input).index(i)+1) for i in _input])
 
 # 871198282
